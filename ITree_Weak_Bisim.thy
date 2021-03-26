6477952bc121ebf3ee629a8e3d6fecf948c15827
simondfoster <simondf@gmail.com>
2021-03-26 15:56:20 +0000

    Switched to coinductive inductive definition for weak bisim

--- ITree_Weak_Bisim.thy
+++ ITree_Weak_Bisim.thy
@@ -4,17 +4,28 @@ begin
 
 subsection \<open> Weak Bisimulation \<close>
 
+inductive wbisim_upto :: "('e, 's) itree \<Rightarrow> (('e, 's) itree \<Rightarrow> ('e, 's) itree \<Rightarrow> bool) \<Rightarrow> ('e, 's) itree \<Rightarrow> bool" (infix "\<approx>\<^bsub>_\<^esub>" 50) where
+wbupto_Ret [intro]: "Ret x \<approx>\<^bsub>R\<^esub> Ret x" |
+wbupto_Sil1 [intro]: "P \<approx>\<^bsub>R\<^esub> Q \<Longrightarrow> Sil P \<approx>\<^bsub>R\<^esub> Q" |
+wbupto_Sil2 [intro]: "P \<approx>\<^bsub>R\<^esub> Q \<Longrightarrow> P \<approx>\<^bsub>R\<^esub> Sil Q" |
+wbisim_Vis [intro]: "\<lbrakk> dom(F) = dom(G); \<And> e. e \<in> dom(F) \<Longrightarrow> R (the (F e)) (the (G e)) \<rbrakk> \<Longrightarrow> Vis F \<approx>\<^bsub>R\<^esub> Vis G"
+
+abbreviation (input) "wbupto R \<equiv> (\<lambda> P Q. P \<approx>\<^bsub>R\<^esub> Q)"
+
+lemma monotonic_wbisim_upto: "P \<approx>\<^bsub>R\<^esub> Q \<Longrightarrow> R \<le> S \<Longrightarrow> P \<approx>\<^bsub>S\<^esub> Q"
+  by (induct rule: wbisim_upto.induct ,auto)
+
+lemma mono_stabilises_to [mono add]: "R \<le> S \<Longrightarrow> wbupto R \<le> wbupto S"
+  by (auto intro: monotonic_wbisim_upto)
+
 coinductive wbisim :: "('e, 's) itree \<Rightarrow> ('e, 's) itree \<Rightarrow> bool" (infix "\<approx>" 50) where
-wbisim_Ret [intro]: "Ret x \<approx> Ret x" |
-wbisim_Sil1 [intro]: "P \<approx> Q \<Longrightarrow> Sil P \<approx> Q" |
-wbisim_Sil2 [intro]: "P \<approx> Q \<Longrightarrow> P \<approx> Sil Q" |
-wbisim_Vis [intro]: "\<lbrakk> dom(F) = dom(G); \<And> e. e \<in> dom(F) \<Longrightarrow> the (F e) \<approx> the (G e) \<rbrakk> \<Longrightarrow> Vis F \<approx> Vis G"
+wbisim: "P \<approx>\<^bsub>wbisim\<^esub> Q \<Longrightarrow> P \<approx> Q"
 
 inductive_cases
   wbisim_RetE: "P \<approx> Ret x"
 
 lemma wbisim_sym [intro]: "P \<approx> Q \<Longrightarrow> Q \<approx> P"
-  by (coinduction arbitrary: P Q, auto elim: wbisim.cases)
+  apply (coinduction arbitrary: P Q, auto) oops
 
 lemma "Sils n P \<approx> Sils n P"
   apply (coinduction arbitrary: n P, auto)
