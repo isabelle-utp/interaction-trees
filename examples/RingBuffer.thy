83d81496b65835ba534c541c85206ce58fa0db51
Simon Foster <simon.foster@york.ac.uk>
2021-04-12 14:01:21 +0100

    Tidying

--- examples/RingBuffer.thy
+++ examples/RingBuffer.thy
@@ -1,10 +1,12 @@
+section \<open> Distributed Ring Buffer \<close>
+
 theory RingBuffer
   imports "../ITree_Circus" "HOL-Library.Code_Target_Nat" "HOL-Library.Code_Target_Int"
 begin
 
-no_notation conj  (infixr "&" 35)
+no_notation conj  (infixr "&" 35) lit_vars
 
-lit_vars
+subsection \<open> Ring Process \<close>
 
 type_synonym RingIndex = nat
 
@@ -28,23 +30,28 @@ definition default_CellState_ext :: "'a CellState_scheme" where
 "default_CellState_ext = CellState.extend (CellState.make 0) default"
 
 instance ..
-
 end
 
-definition "Read(i) = rd!((i,val)) \<rightarrow> Skip"
+definition Read :: "nat \<Rightarrow> (chan, CellState) action" where
+"Read(i) = rd!((i,val)) \<rightarrow> Skip"
 
-definition "Write(i) = wrt?(_,v):(map (\<lambda> v. (i, v)) [0,1,2,3]) \<rightarrow> val := v"
+definition Write :: "nat \<Rightarrow> (chan, CellState) action" where
+"Write(i) = wrt?(_,v):(map (\<lambda> v. (i, v)) [0,1,2,3]) \<rightarrow> val := v"
 
-definition "IRingCell(i) = proc ([val \<leadsto> 0] :: CellState subst) (Write(i) \<Zcomp> loop (Read(i) \<box> Write(i)))"
+definition IRingCell :: "nat \<Rightarrow> chan process" where
+"IRingCell(i) = proc ([val \<leadsto> 0] :: CellState subst) (Write(i) \<Zcomp> loop (Read(i) \<box> Write(i)))"
 
-definition "Ring = foldl (\<interleave>) (Ret ()) (map (IRingCell \<circ> nat) [0..maxbuff])"
+definition Ring :: "chan process" where
+"Ring = foldl (\<interleave>) (Ret ()) (map (IRingCell \<circ> nat) [0..maxbuff])"
+
+subsection \<open> Controller Process \<close>
 
 alphabet ControllerState =
-  sz :: nat
+  sz       :: nat
   ringsize :: integer
-  cache :: integer
-  rtop :: RingIndex
-  rbot :: RingIndex
+  cache    :: integer
+  rtop     :: RingIndex
+  rbot     :: RingIndex
 
 instantiation ControllerState_ext :: (default) default
 begin
@@ -57,33 +64,40 @@ instance ..
 end
 
 
-definition "InitController = [sz \<leadsto> 0, rbot \<leadsto> 0, rtop \<leadsto> 0]"
+definition InitController :: "ControllerState subst" where
+"InitController = [sz \<leadsto> 0, rbot \<leadsto> 0, rtop \<leadsto> 0]"
 
-definition "CacheInput x = (sz := 1 \<Zcomp> cache := x)"
 
-definition "StoreInput = (sz := (sz + 1) \<Zcomp> rtop := ((rtop + 1) mod maxring))"
+definition CacheInput :: "integer \<Rightarrow> (chan, ControllerState) action" where
+"CacheInput x = (sz := 1 \<Zcomp> cache := x)"
 
-definition "InputController = ((sz < maxbuff) & input?(x):[0,1,2,3] \<rightarrow> 
+definition StoreInput :: "(chan, ControllerState) action" where
+"StoreInput = (sz := (sz + 1) \<Zcomp> rtop := ((rtop + 1) mod maxring))"
+
+definition InputController :: "(chan, ControllerState) action" where
+  "InputController = ((sz < maxbuff) & input?(x):[0,1,2,3] \<rightarrow> 
                                 ((sz = 0) & CacheInput x 
                                 \<box> (sz > 0) & wrt!((rtop, x)) \<rightarrow> StoreInput))"
 
-definition "NoNewCache = (sz := 1)"
+definition NoNewCache :: "(chan, ControllerState) action" where
+"NoNewCache = (sz := 0)"
 
-definition "StoreNewCache x = sz := (sz - 1) \<Zcomp> cache := x \<Zcomp> rbot := ((rbot + 1) mod maxring)"
+definition StoreNewCache :: "integer \<Rightarrow> (chan, ControllerState) action" where
+"StoreNewCache x = sz := (sz - 1) \<Zcomp> cache := x \<Zcomp> rbot := ((rbot + 1) mod maxring)"
 
-definition "OutputController = (sz > 0) & output!(cache) \<rightarrow> 
+definition OutputController :: "(chan, ControllerState) action" where
+  "OutputController = (sz > 0) & output!(cache) \<rightarrow> 
                                   ((sz > 1) & rd?(_, x):(map (\<lambda> i. (rbot, i)) [0,1,2,3]) \<rightarrow> StoreNewCache x
                                    \<box> (sz = 1) & NoNewCache)"
 
-definition "Controller = proc (InitController :: ControllerState \<Rightarrow> ControllerState) (loop (InputController \<box> OutputController))"
+definition Controller :: "chan process" where
+"Controller = proc (InitController :: ControllerState \<Rightarrow> ControllerState) (loop (InputController \<box> OutputController))"
 
-term "[Read_C (i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]] @ [Write_C (i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]]"
-
-definition 
+definition CRing :: "chan process" where
   "CRing = hide (Controller 
            \<parallel>\<^bsub>set ([rd_C (nat i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]] @ [wrt_C (nat i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]])\<^esub> 
            Ring) (set ([rd_C (nat i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]] @ [wrt_C (nat i, v). i \<leftarrow> [0..maxbuff], v \<leftarrow> [0,1,2,3]]))"
 
-export_code Ring Controller CRing in Haskell module_name RingBuffer (string_classes)
+export_code Ring Controller CRing in Haskell module_name RingBuffer file_prefix RingBuffer (string_classes) 
 
 end
