5bcfd452ebbd9010d70c7cfd7cfb410628e04903
Simon Foster <simon.foster@york.ac.uk>
2021-06-11 14:26:07 +0100

    Added a small utility for generating instances of the "default" class for records

---
+++ ITree_Extraction.thy
@@ -0,0 +1,19 @@
+subsection \<open> ITree Code Generation Support \<close>
+
+theory ITree_Extraction
+  imports ITree_Circus Record_Default_Instance
+begin
+
+instantiation set :: (type) default
+begin
+definition "default_set = ({} :: 'a set)"
+instance ..
+end
+
+instantiation pfun :: (type, type) default
+begin
+definition "default_pfun = ({}\<^sub>p :: ('a, 'b) pfun)"
+instance ..
+end
+
+end
