cb6285876c11fb8ddbb644054a2c110ed0c1933a
Simon Foster <simon.foster@york.ac.uk>
2021-09-02 09:44:12 +0100

    Fixed FileSystem

--- examples/FileSystem.thy
+++ examples/FileSystem.thy
@@ -25,26 +25,26 @@ chantype fchan =
   fileContent :: "Data"
   writeFile :: "Key \<times> Data"
 
+(*
 definition Read\<^sub>0 :: "('e, integer, integer, File) procedure" where
 "Read\<^sub>0 = (proc (k :: integer). \<questiondown>k \<in> pdom contents? \<Zcomp> return (contents k))"
 
 term "operation readFile fileContent Read\<^sub>0"
-
-(*
-definition Read\<^sub>0 :: "(fchan, File) htree" where
-"Read\<^sub>0 = readFile?(k):(pdom contents) \<rightarrow> fileContent!(contents k) \<rightarrow> Skip"
 *)
 
+definition Read\<^sub>0 :: "(fchan, File) htree" where
+"Read\<^sub>0 = readFile?(k):(pdom contents) \<rightarrow> fileContent!(contents k) \<rightarrow> Skip"
+
 definition Write\<^sub>0 :: "(fchan, File) htree" where
-"Write\<^sub>0 = writeFile?(k, d):(pdom contents \<times> Data) \<rightarrow> contents := contents + {k \<mapsto> d}\<^sub>p"
+"Write\<^sub>0 = writeFile?(k, d):(pdom contents \<times> Data) \<rightarrow> contents := contents \<oplus> {k \<mapsto> d}\<^sub>p"
 
 definition Add\<^sub>0 :: "(fchan, File) htree" where
-"Add\<^sub>0 = addFile?(k, d):((Key - pdom contents) \<times> Data) \<rightarrow> contents := contents + {k \<mapsto> d}\<^sub>p"
+"Add\<^sub>0 = addFile?(k, d):((Key - pdom contents) \<times> Data) \<rightarrow> contents := contents \<oplus> {k \<mapsto> d}\<^sub>p"
 
 definition Delete\<^sub>0 :: "(fchan, File) htree" where
 "Delete\<^sub>0 = deleteFile?(k):(pdom contents) \<rightarrow> contents := {k} \<Zndres> contents"
 
-definition "SingleFile = process FileInit (loop (operation readFile fileContent Read\<^sub>0 \<box> Write\<^sub>0 \<box> Add\<^sub>0 \<box> Delete\<^sub>0))"
+definition "SingleFile = process FileInit (loop (Read\<^sub>0 \<box> Write\<^sub>0 \<box> Add\<^sub>0 \<box> Delete\<^sub>0))"
 
 export_code SingleFile in Haskell module_name SingleFile (string_classes)
 
@@ -60,7 +60,10 @@ schema System =
 
 
 term Read\<^sub>0
+
+(*
 term "promote_proc Read\<^sub>0 (\<lambda> i. collection_lens i ;\<^sub>L file)"
+*)
 
 definition "SystemInit = [file \<leadsto> {}\<^sub>p]"
 
