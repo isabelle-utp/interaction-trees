d2e64a2b631d232649c11e8b9eb33296b017685c
Simon Foster <simon.foster@york.ac.uk>
2021-09-16 14:54:26 +0100

    Added missing meta-theory

---
+++ ITrees.thy
@@ -0,0 +1,7 @@
+section \<open> Interaction Trees Meta-Theory \<close>
+
+theory ITrees
+  imports 
+    Interaction_Trees ITree_Divergence ITree_Deadlock ITree_Iteration ITree_Weak_Bisim 
+    ITree_Eval ITree_Extraction ITree_Simulation 
+begin end
