0839ee0e8340ef27127ce11c94ba83d95c9fc6d5
Simon Foster <simon.foster@york.ac.uk>
2024-05-23 13:01:53 +0100

    Simpler invariant and proof for Gnome sort

--- UTP/VCG/examples/Gnome_Sort.thy
+++ UTP/VCG/examples/Gnome_Sort.thy
@@ -10,14 +10,13 @@ zstore gnome_st =
 
 program gnome_sort "xs :: int list" over gnome_st
   = "a := xs; pos := 0; 
-     while pos < length a 
-     inv 0 \<le> pos \<and> pos \<le> length a 
-       \<and> sorted (nths a {0..<pos})
-       \<and> mset a = mset xs 
+     while pos < length a
+     inv sorted (take pos a) \<and> mset a = mset xs 
      do
        if (pos = 0 \<or> a ! pos \<ge> a ! (pos - 1))
        then pos := pos + 1
-       else (a[pos], a[pos-1]) := (a ! (pos-1), a!pos); pos := pos - 1
+       else (a[pos], a[pos-1]) := (a ! (pos-1), a ! pos); 
+            pos := pos - 1
        fi
      od"
 
@@ -25,11 +24,10 @@ execute "gnome_sort [5,3,5,4,2]"
 
 lemma gnome_sort_correct: "H{True} gnome_sort xs {sorted a \<and> mset xs = mset a}"
   apply vcg
-  apply (simp add: nths_single)
+  using length_greater_0_conv take_Suc_conv_app_nth apply fastforce
   apply (smt (verit) One_nat_def Suc_less_eq cancel_comm_monoid_add_class.diff_zero diff_Suc_1 less_Suc_eq less_or_eq_imp_le nth_take nths_atLeastLessThan_0_take nths_upt_length sorted_iff_nth_Suc zero_less_Suc)
-  apply (metis atLeastLessThan_iff diff_le_self dual_order.strict_trans2 less_not_refl nths_list_update_out sorted_nths_atLeastLessThan_0)
+  apply (metis One_nat_def Suc_diff_1 less_imp_diff_less sorted_append_middle take_hd_drop)
   apply (simp add: mset_swap)
-  apply (simp add: nths_all)
   done
 
 end
